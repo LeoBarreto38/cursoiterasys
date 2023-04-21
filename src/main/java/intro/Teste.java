68a9c1cc5ae16e1de16611561a32d651b7fe706b
leoro <leorodrigo232@gmail.com>
2023-04-21 18:30:58 -0300

    Medidas

--- src/main/java/intro/Teste.java
+++ src/main/java/intro/Teste.java
@@ -1,21 +1,18 @@
+// 1 - Pacote
 package intro;
 
 public class Teste {
 
    public static void main(String[] args){
-       System.out.println("Cálculo de areas");
+       System.out.println("Bom dia !!!");
+       System.out.println(" Esse é o álculo de areas");
 
-       // Calculo de área - Exemplo: o tamanho do tapete ou do piso
-       int largura;
-       int comprimento;
-       int resultado;
+       // Calculo de área - Reduzido
+       int largura = 4;
+       int comprimento = 3;
 
-       largura = 5; // largura recebe 4
-       comprimento = 6;
-
-       resultado = largura * comprimento;
        System.out.println(" Para a largura de " + largura + "m e o comprimento "
-               + comprimento + "m A área é de " + resultado + "m²");
+               + comprimento + "m A área é de " + largura * comprimento + "m²");
 
    }
 }
