3cc4353d05c075d70d1f13154209ba8d4277d71d
leoro <leorodrigo232@gmail.com>
2023-04-20 21:01:34 -0300

    Medeidas

---
+++ src/main/java/intro/Teste.java
@@ -0,0 +1,21 @@
+package intro;
+
+public class Teste {
+
+   public static void main(String[] args){
+       System.out.println("Cálculo de areas");
+
+       // Calculo de área - Exemplo: o tamanho do tapete ou do piso
+       int largura;
+       int comprimento;
+       int resultado;
+
+       largura = 5; // largura recebe 4
+       comprimento = 6;
+
+       resultado = largura * comprimento;
+       System.out.println(" Para a largura de " + largura + "m e o comprimento "
+               + comprimento + "m A área é de " + resultado + "m²");
+
+   }
+}
