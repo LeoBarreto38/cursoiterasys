14a1caae72014169b79f880d3341b2228eb79d35
leoro <leorodrigo232@gmail.com>
2023-04-21 21:09:15 -0300

    Exclui o "BOm dia !!!" no começo da impressão de testo no console
    Criei o método calculoAreaModoCurto e movi o código que estava em main para lá
    
    Alterei o metodo main agora chama  os 2 métodos calcular* que passaram também a ser static

--- src/main/java/intro/Teste.java
+++ src/main/java/intro/Teste.java
@@ -2,17 +2,40 @@
 package intro;
 
 public class Teste {
+     // 3.1 - Atributo - Caracteristicas
 
-   public static void main(String[] args){
-       System.out.println("Bom dia !!!");
-       System.out.println(" Esse é o álculo de areas");
+    // 3.2 - Métodos e funções
+   public static void main(String[] args) {
 
-       // Calculo de área - Reduzido
+       calcularAreaModoExtenso();
+       calcularAreaModoCurto();
+   }
+
+       public static void calcularAreaModoCurto(){
+       System.out.println("Cálculo de areas Modo curto");
+
+       // Calculo de area - Reduzido
        int largura = 4;
        int comprimento = 3;
 
-       System.out.println(" Para a largura de " + largura + "m e o comprimento "
+       System.out.println(" Para a largura de " + largura + "m e o comprimento de "
                + comprimento + "m A área é de " + largura * comprimento + "m²");
+
+   }
+
+public static void  calcularAreaModoExtenso(){
+    System.out.println("Cálculo de areas Modo Extenso");
+       // Calculo de área - Exemplo: o tamanho do tapete ou do piso
+       int largura;
+       int comprimento;
+       int resultado;
+
+       largura = 5; // largura recebe 4
+       comprimento = 6;
+
+       resultado = largura * comprimento;
+       System.out.println(" Para a largura de " + largura + "m e o comprimento de "
+               + comprimento + "m A área é de " + resultado + "m²");
 
    }
 }
